ca1515cd816ca6a4629d504dea5eff4808c03acc
dannycampa <me@dannycampa.com>
2018-08-21 22:57:05 -0700

    linting

--- src/common-ui/components/container/component.tsx
+++ src/common-ui/components/container/component.tsx
@@ -5,54 +5,55 @@ type SpacingSize = 0 | 0.5 | 1 | 2 | 3 | 4 | 5;
 type Spacings = SpacingSize | { x?: SpacingSize, y?: SpacingSize, top?: SpacingSize, right?: SpacingSize, bottom?: SpacingSize, left?: SpacingSize };
 
 interface PublicProps {
-    padding?: Spacings;
-    margin?: Spacings;
+  padding?: Spacings;
+  margin?: Spacings;
 }
 
 type Props = React.Props<Container> & PublicProps;
 
 export class Container extends React.Component<Props, React.ComponentState> {
-    public render() {
-        let classes: string[] = [];
+  public render() {
+    let classes: string[] = [];
 
-        classes = classes.concat(this.getSpacingClasses())
+    classes = classes.concat(this.getSpacingClasses());
 
-        return (
-            <div className={classes.join(' ')}>
-                {this.props.children}
-            </div>
-        );
+    return (
+      <div className={classes.join(' ')}>
+        {this.props.children}
+      </div>
+    );
+  }
+
+  private getSpacingClasses = () => {
+    const { padding, margin } = this.props;
+
+    if (padding) {
+      return this.getSpaceValue('pd', padding);
     }
 
-    private getSpacingClasses = () => {
-        const { padding, margin } = this.props;
-
-        if (padding) {
-            return this.getSpaceValue('pd', padding);
-        }
-
-        if (margin) {
-            return this.getSpaceValue('mg', margin);
-        }
-
-        return [];
+    if (margin) {
+      return this.getSpaceValue('mg', margin);
     }
 
-    private getSpaceValue = (prefix: 'pd' | 'mg', spacing: Spacings) => {
-        if (typeof spacing === 'number') {
-            return [`${prefix}-${spacing}`];
-        }
+    return [];
+  }
 
-        if (typeof spacing === 'object') {
-            const values: string[] = [];
-            Object.keys(spacing).forEach((key) => {
-                const value: string = `${(spacing as any)[key]}`;
-                const parsedValue = value.split('.').join('');
-                values.push(`${prefix}-${key.substr(0, 1)}-${parsedValue}`);
-            });
-            return values;
-        }
-
-        return [];
+  private getSpaceValue = (prefix: 'pd' | 'mg', spacing: Spacings) => {
+    if (typeof spacing === 'number') {
+      return [`${prefix}-${spacing}`];
     }
+
+    if (typeof spacing === 'object') {
+      const values: string[] = [];
+      Object.keys(spacing).forEach((key) => {
+        // tslint:disable-next-line:no-any
+        const value: string = `${(spacing as any)[key]}`;
+        const parsedValue = value.split('.').join('');
+        values.push(`${prefix}-${key.substr(0, 1)}-${parsedValue}`);
+      });
+      return values;
+    }
+
+    return [];
+  }
 }
--- src/core/components/root/component.tsx
+++ src/core/components/root/component.tsx
@@ -1,18 +1,18 @@
 
-import * as React from 'react';
-import 'normalize.css';
-import './styles.scss';
 import { Container } from '@ladders/common-ui';
+import 'normalize.css';
+import * as React from 'react';
+import './styles.scss';
 
 type Props = React.Props<Root>;
 
 export class Root extends React.Component<Props, React.ComponentState> {
-    public render() {
-        return (
-            <Container padding={{x: 1, y: 2}}>
-                <h1>Hello World</h1>
-                <p>Sweet</p>
-            </Container>
-        );
-    }
+  public render() {
+    return (
+      <Container padding={{ x: 1, y: 2 }}>
+        <h1>Hello World</h1>
+        <p>Sweet</p>
+      </Container>
+    );
+  }
 }
